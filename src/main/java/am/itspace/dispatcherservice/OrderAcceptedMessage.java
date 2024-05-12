974fdb6f7fc0d86e669cd7caf77d1ba234ca87cc
Mr_Artur <panosyanartur31@gmail.com>
2024-05-12 15:44:48 +0400

    first commit

---
+++ src/main/java/am/itspace/dispatcherservice/OrderAcceptedMessage.java
@@ -0,0 +1,7 @@
+package am.itspace.dispatcherservice;
+
+public record OrderAcceptedMessage(
+
+        Long orderId
+) {
+}
---
+++ src/main/java/am/itspace/dispatcherservice/OrderDispatchedMessage.java
@@ -0,0 +1,7 @@
+package am.itspace.dispatcherservice;
+
+public record OrderDispatchedMessage(
+
+        Long orderId
+) {
+}
